c252755d65876113dc420230b8fc02642b56dd5a
almog-cohen95 <almogc@moveohls.com>
2025-01-26 14:48:48 +0200

    Fixed schema- remove notes

--- src/pokemon/schemas/pokemon.schema.ts
+++ src/pokemon/schemas/pokemon.schema.ts
@@ -1,44 +1,3 @@
-// import mongoose from 'mongoose';
-
-// export const PokemonSchema = new mongoose.Schema({
-//   id: Number,
-//   name: {
-//     english: String,
-//     japanese: String,
-//     chinese: String,
-//     french: String,
-//   },
-//   type: [String],
-//   base: {
-//     HP: Number,
-//     Attack: Number,
-//     Defense: Number,
-//     SpAttack: Number,
-//     SpDefense: Number,
-//     Speed: Number,
-//   },
-//   species: String,
-//   description: String,
-//   evolution: {
-//     prev: [String],
-//     next: [[String]],
-//   },
-//   profile: {
-//     height: String,
-//     weight: String,
-//     egg: [String],
-//     ability: [[String]],
-//     gender: String,
-//   },
-//   image: {
-//     sprite: String,
-//     thumbnail: String,
-//     hires: String,
-//   },
-// });
-
-// export const PokemonModelName = 'Pokemon';
-
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
@@ -46,8 +5,6 @@ export type PokemonDocument = Pokemon & Document;
 
 @Schema({collection: 'pokemon'})
 export class Pokemon {
-  // @Prop({ required: true })
-  // id: number;
 
   @Prop({
     type: {
